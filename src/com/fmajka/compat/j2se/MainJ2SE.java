e609368d737064bd623b21f8a0b070b30e971a88
Franciszek Majka <franiumaju@gmail.com>
2025-09-19 00:09:33 +0200

    Moved main to its respective folder, removed unused imports

--- src/com/fmajka/compat/j2se/MainJ2SE.java
+++ src/com/fmajka/compat/j2se/MainJ2SE.java
@@ -1,11 +1,11 @@
+package com.fmajka.compat.j2se;
+
 import com.fmajka.compat.Context;
-import com.fmajka.compat.j2se.GraphicsJ2SE;
-import com.fmajka.compat.j2se.MediaImageFactoryJ2SE;
 import com.fmajka.compat.j2se.gui.GameFrame;
 
 import shinmadan.tohoSINMA;
 
-public class LauncherPC {
+public class MainJ2SE {
     public static void main(String[] args) {
         GameFrame gameFrame = new GameFrame();
         Context.init(new GraphicsJ2SE(gameFrame.panel), new MediaImageFactoryJ2SE());
