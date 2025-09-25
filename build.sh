4e80e3d789849cb315e171997f1f8dd2ff30eadb
Franciszek Majka <franiumaju@gmail.com>
2025-09-25 16:43:50 +0200

    The build file now returns an exit status

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-(javac -Xmaxerrs 1 -d bin/pc $(find . -name "*.java") && echo "Build successful" || echo "Build failed!") && (jar cfe sinma.jar com.fmajka.compat.j2se.MainJ2SE -C bin/pc . && echo "Jar successful!" || echo "Jar failed!")
+status=0
+
+if javac -Xmaxerrs 1 -d bin/pc $(find . -name "*.java"); then
+    echo "Build successful"
+else
+    echo "Build failed!"
+    status=1
+fi
+
+if [ $status -eq 0 ]; then
+    if jar cfe sinma.jar com.fmajka.compat.j2se.MainJ2SE -C bin/pc .; then
+        echo "Jar successful!"
+    else
+        echo "Jar failed!"
+        status=1
+    fi
+fi
+
+exit $status
